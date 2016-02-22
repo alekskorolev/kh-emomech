7ed0221e0f3be6e08990a69f1c6b80751b5b0512
Aleksandr Korolev <aleksandr@korolev.email>
2016-02-22 03:16:40 +0300

    button action

--- cs/source/js/components/search/index.js
+++ cs/source/js/components/search/index.js
@@ -8,14 +8,19 @@ import './style.scss';
 class SearchComponent extends BaseComponent {
     beforeInitialize(options) {
         this.delegateEvents({
-            'keyup .jsc-input': 'input'
+            'keyup .jsc-input': 'input',
+            'click .jsc-check': 'click'
         });
         this.template = template;
         super.beforeInitialize(options);
     }
+    click(event) {
+        event.value = this.$('.jsc-input').val();
+        this.jTrigger('action.search', event);
+    }
     input(event) {
         if (event.keyCode === 13) {
-            event.value = $(event.currentTarget).val()
+            event.value = $(event.currentTarget).val();
             this.jTrigger('action.search', event);
         }
     }
